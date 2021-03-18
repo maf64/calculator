961fd603392181dd7a6a9fbe7581a656c43708a3
maf74 <maf64@njit.edu>
2021-03-18 13:09:29 -0400

    Feature: Setup calculator class and test source code

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,12 +1,3 @@
-def addition(a, b):
-    c = a + b
-    return c
-
-
-def subtraction(a, b):
-    c = a - b
-    return c
-
 
 class Calculator:
     result = 0
@@ -14,10 +5,3 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, a, b):
-        self.result = addition(a, b)
-        return self.result
-
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.result
